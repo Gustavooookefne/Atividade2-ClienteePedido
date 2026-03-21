f9923299370e147f85e082206b4eff467e881a96
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 21:25:23 -0300

    feat: Finalizado o PedidosRepository

--- Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/repository/PedidoRepository.java
+++ Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/repository/PedidoRepository.java
@@ -1,4 +1,10 @@
 package com.Weg.ClienteePedido.repository;
 
-public class PedidoRepository {
+import com.Weg.ClienteePedido.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PedidoRepository extends JpaRepository<Pedido , UUID> {
+
 }
