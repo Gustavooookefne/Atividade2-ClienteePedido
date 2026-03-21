60a2ddc3a5618df3c09fdbb28e34022d1d17f01d
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 21:02:59 -0300

    Add files via upload

---
+++ Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/ClienteePedidoApplication.java
@@ -0,0 +1,13 @@
+package com.Weg.ClienteePedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteePedidoApplication.class, args);
+	}
+
+}
---
+++ Atividade2-ClienteePedido/ClienteePedido/src/test/java/com/Weg/ClienteePedido/ClienteePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Weg.ClienteePedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
