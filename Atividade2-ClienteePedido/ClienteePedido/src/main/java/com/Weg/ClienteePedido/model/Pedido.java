fed3cf809f1a2a22a2be119817266ad216d4810e
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 21:13:52 -0300

    feat: Criando class pedido

--- Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/model/Pedido.java
+++ Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/model/Pedido.java
@@ -1,4 +1,25 @@
 package com.Weg.ClienteePedido.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "data_entrega" , nullable = false)
+    private LocalDate dataDeEntrega;
+
+
 }
