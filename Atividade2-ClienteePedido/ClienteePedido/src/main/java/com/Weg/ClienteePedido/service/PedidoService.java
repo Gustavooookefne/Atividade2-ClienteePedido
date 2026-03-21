49cbe443021ce0ee0519a68d48769faf36335da4
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-20 21:54:34 -0300

    feat: Terminar PedidoService

--- Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/service/PedidoService.java
+++ Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/service/PedidoService.java
@@ -1,5 +1,24 @@
 package com.Weg.ClienteePedido.service;
 
+import com.Weg.ClienteePedido.dto.PedidoDto.PedidoRequestDto;
+import com.Weg.ClienteePedido.dto.PedidoDto.PedidoResponseDto;
+import com.Weg.ClienteePedido.mapper.PedidoMapper;
+import com.Weg.ClienteePedido.model.Pedido;
+import com.Weg.ClienteePedido.repository.PedidoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class PedidoService {
-    private
+
+    private final PedidoRepository repository;
+    private final PedidoMapper mapper;
+
+    public PedidoResponseDto salvar (PedidoRequestDto requestDto) {
+
+        Pedido pedido = new Pedido();
+
+        return mapper.paraDto(repository.save(pedido));
+    }
 }
