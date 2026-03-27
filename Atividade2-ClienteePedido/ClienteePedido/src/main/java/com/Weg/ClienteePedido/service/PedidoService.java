1d94ebec3ee611ddd8bee9ada0b8ab85bdc76a2b
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 13:59:03 -0300

    feat: Finalizado o PedidoService

--- Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/service/PedidoService.java
+++ Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/service/PedidoService.java
@@ -8,6 +8,9 @@ import com.Weg.ClienteePedido.repository.PedidoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class PedidoService {
@@ -20,5 +23,33 @@ public class PedidoService {
         Pedido pedido = new Pedido();
 
         return mapper.paraDto(repository.save(pedido));
+    }
+
+    public List<PedidoResponseDto> listarTodos(){
+        return repository.findAll()
+                .stream()
+                .map(mapper::paraDto)
+                .toList();
+    }
+
+    public PedidoResponseDto listarPorId (UUID id) {
+        Pedido pedido = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Lista não encontrada"));
+
+        return mapper.paraDto(pedido);
+    }
+
+    public PedidoResponseDto atualizar (PedidoRequestDto requestDto, UUID id) {
+        Pedido pedido = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+
+        pedido.setDescricao(requestDto.descricao());
+        pedido.setDataDeEntrega(requestDto.dataDeEntrega());
+
+        return mapper.paraDto(pedido);
+    }
+
+    public void delete (UUID id) {
+        repository.deleteById(id);
     }
 }
