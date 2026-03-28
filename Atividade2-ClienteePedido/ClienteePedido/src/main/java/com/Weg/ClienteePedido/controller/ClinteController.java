489cffa3e56fa5bbc8989c17bdfe193fa7d7dbd6
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 21:27:15 -0300

    feat: Adicionando RepnseEntity

--- Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/controller/ClinteController.java
+++ Atividade2-ClienteePedido/ClienteePedido/src/main/java/com/Weg/ClienteePedido/controller/ClinteController.java
@@ -4,6 +4,8 @@ import com.Weg.ClienteePedido.dto.ClienteDto.ClienteRequestDto;
 import com.Weg.ClienteePedido.dto.ClienteDto.ClienteResponseDto;
 import com.Weg.ClienteePedido.service.ClienteService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +18,35 @@ public class ClinteController {
     private final ClienteService service;
 
     @PutMapping
-    public ClienteResponseDto salvar (@RequestBody ClienteRequestDto requestDto) {
-        return service.salvar(requestDto);
+    public ResponseEntity<ClienteResponseDto> salvar (@RequestBody ClienteRequestDto requestDto) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(service.salvar(requestDto));
     }
 
     @GetMapping
-    public List<ClienteResponseDto> listarTodos () {
-        return service.listarTodos();
+    public ResponseEntity<List<ClienteResponseDto>> listarTodos () {
+            List<ClienteResponseDto> responseDtos = service.listarTodos();
+
+        return ResponseEntity.ok().body(responseDtos);
     }
 
     @GetMapping("/{id}")
-    public ClienteResponseDto listarPorId (@PathVariable Long id) {
-        return service.listarPorId(id);
+    public ResponseEntity<ClienteResponseDto> listarPorId (@PathVariable Long id) {
+            ClienteResponseDto responseDto = service.listarPorId(id);
+
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @PostMapping("/{id}")
-    public ClienteResponseDto atualizar (@RequestBody ClienteRequestDto requestDto , @PathVariable Long id) {
-        return service.atualizar(requestDto , id);
+    public ResponseEntity<ClienteResponseDto> atualizar (@RequestBody ClienteRequestDto requestDto , @PathVariable Long id) {
+            ClienteResponseDto responseDto = service.atualizar(requestDto , id);
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @DeleteMapping("/{id}")
-    public void deletar (Long id) {
+    public ResponseEntity<Void> deletar (Long id) {
         service.deletar(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
